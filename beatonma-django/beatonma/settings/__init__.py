1a2819b4c60b70afc4af6b04216c05e5fcf967e6
Michael Beaton <beatonma@gmail.com>
2023-08-15 14:31:23 +0100

    Set login redirects to site homepage.

--- beatonma-django/beatonma/settings/__init__.py
+++ beatonma-django/beatonma/settings/__init__.py
@@ -55,3 +55,5 @@ CSRF_TRUSTED_ORIGINS = [
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 TEST_RUNNER = "basetest.testrunner.PytestTestRunner"
+LOGIN_URL = "/"
+LOGIN_REDIRECT_URL = "/"
