9ba69a9be074634ff46a5a64926628520dd78af5
Michael Beaton <beatonma@gmail.com>
2023-08-16 13:38:47 +0100

    Use settings.SITE_NAME in admin pages.

--- beatonma-django/main/admin/__init__.py
+++ beatonma-django/main/admin/__init__.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.contrib import admin
 
 from .flatpages import *
 from .models import *
 
-admin.site.site_header = "beatonma.org"
+admin.site.site_header = settings.SITE_NAME
+admin.site.site_title = settings.SITE_NAME
 admin.site.index_title = "Index"
-admin.site.site_title = "beatonma.org"
