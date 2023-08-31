ee4c6a2eaddc453955f6c7e4ac98d6e2917f08f2
Michael Beaton <beatonma@gmail.com>
2023-08-31 14:11:50 +0100

    Added separate `package.json` for cypress module, mostly just to make pycharm happier.

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "cypress";
 
+const defaultTimeout = 2_000;
+
 export default defineConfig({
-    chromeWebSecurity: false,
-    e2e: {
-        experimentalRunAllSpecs: true,
-        supportFile: false,
-    },
-    trashAssetsBeforeRuns: false, // Keep screenshots
+  chromeWebSecurity: false,
+  e2e: {
+    experimentalRunAllSpecs: true,
+    supportFile: false,
+  },
+  trashAssetsBeforeRuns: false, // Keep screenshots,
+  defaultCommandTimeout: defaultTimeout,
+  taskTimeout: 10_000,
+  execTimeout: 5_000,
+  pageLoadTimeout: defaultTimeout,
+  requestTimeout: defaultTimeout,
+  responseTimeout: defaultTimeout,
 });
