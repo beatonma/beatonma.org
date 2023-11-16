a314469e593ecca527dbc71dfe036e2921be8f10
Michael Beaton <beatonma@gmail.com>
2023-11-16 14:28:40 +0000

    Log shell commands and their output.

--- beatonma-gulp/gulpfile.ts/build/util.ts
+++ beatonma-gulp/gulpfile.ts/build/util.ts
@@ -2,14 +2,15 @@ import child_process from "child_process";
 
 export const shell_command = (command: string, cwd: string = ".") =>
     new Promise<void>((resolve, reject) => {
+        console.info(`> ${command} [${cwd}]`);
         child_process.exec(
             command,
             { cwd: cwd, timeout: 90_000 },
             (error, stdout, stderr) => {
+                if (stdout) console.debug(stdout);
                 if (error) {
                     console.error(`${command}: ${error}`);
                     if (stderr) console.error(stderr);
-                    if (stdout) console.error(stdout);
                     return reject(error);
                 }
                 resolve();
