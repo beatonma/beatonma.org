c6a4780baf55baf2ead37757956595a8cfc0cccd
Michael Beaton <beatonma@gmail.com>
2023-12-13 18:25:05 +0000

    Removed |note_text filter.
    HTML tags no longer stripped completely from feed note text.
    Instead, whitespace effects of <p> and lists reduced with css.

--- beatonma-django/main/templatetags/page/post.py
+++ beatonma-django/main/templatetags/page/post.py
@@ -1,23 +1,12 @@
 import re
 
 from django import template
-from django.template.defaultfilters import stringfilter
-from django.utils.safestring import mark_safe
 from main.views import reverse
 from taggit.models import Tag
 
 register = template.Library()
 
 html_tag_pattern = re.compile(r"<.*?/?>")
-
-
-@register.filter("note_text")
-@stringfilter
-@mark_safe
-def note_text(html: str) -> str:
-    """Strip HTML tags, except <a>links</a>."""
-    pattern = re.compile(r"<(?!/?a\s*[^>]*>)[^>]*>")
-    return re.sub(pattern, "", html)
 
 
 @register.filter(name="tag_url")
