03ca02542a6d132cb40b2b84e5bdd572ea522fc5
Michael Beaton <beatonma@gmail.com>
2023-12-26 15:56:19 +0000

    Removed method check when removing mentions keys from kwargs.

--- beatonma-django/common/views/mentionable.py
+++ beatonma-django/common/views/mentionable.py
@@ -5,13 +5,12 @@ from mentions import contract
 
 class MentionableView(BaseView):
     def dispatch(self, request: HttpRequest, *args, **kwargs):
-        if request.method == "GET":
-            for key in [
-                contract.URLPATTERNS_MODEL_NAME,
-                contract.URLPATTERNS_MODEL_FILTERS,
-                contract.URLPATTERNS_MODEL_FILTER_MAP,
-            ]:
-                if key in kwargs:
-                    kwargs.pop(key)
+        for key in [
+            contract.URLPATTERNS_MODEL_NAME,
+            contract.URLPATTERNS_MODEL_FILTERS,
+            contract.URLPATTERNS_MODEL_FILTER_MAP,
+        ]:
+            if key in kwargs:
+                kwargs.pop(key)
 
         return super().dispatch(request, *args, **kwargs)
