de77a85c93ee7062222250a3011f7d2f74ba658e
Michael Beaton <beatonma@gmail.com>
2024-02-29 18:16:14 +0000

    Added model `WebappResource` to allow additional resources for a webapp.
    If present, the webapp script will be stored alongside these resources in
    a directory named from the Webapp slug.

--- beatonma-django/main/admin/models/webapp.py
+++ beatonma-django/main/admin/models/webapp.py
@@ -1,9 +1,14 @@
+from common.admin import BaseAdmin
 from django import forms
 from django.contrib import admin
-
-from common.admin import BaseAdmin
 from main.admin.models.posts.webpost import WebPostAdminForm
 from main.models import WebApp
+from main.models.webapp import WebappResource
+
+
+class WebappResourceInline(admin.StackedInline):
+    model = WebappResource
+    extra = 1
 
 
 class WebAppAdminForm(forms.ModelForm):
@@ -18,3 +23,4 @@ class WebAppAdmin(BaseAdmin):
     list_display = [
         "title",
     ]
+    inlines = [WebappResourceInline]
--- beatonma-django/main/models/webapp.py
+++ beatonma-django/main/models/webapp.py
@@ -1,9 +1,36 @@
+import os
+
 from common.models import BaseModel
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from main.storage import OverwriteStorage
 from main.views import view_names
+
+WEBAPPS_UPLOAD_PATH = "webapps/"
+
+
+def _resource_upload_to(instance: "WebappResource", filename):
+    """Dynamically resolve directory for webapp."""
+    return os.path.join(WEBAPPS_UPLOAD_PATH, instance.webapp.slug, filename)
+
+
+def _webapp_resource_dir(webapp_name: str):
+    return os.path.join(settings.MEDIA_ROOT, WEBAPPS_UPLOAD_PATH, webapp_name)
+
+
+class WebappResource(BaseModel):
+    webapp = models.ForeignKey(
+        "WebApp", on_delete=models.CASCADE, related_name="resources"
+    )
+    file = models.FileField(upload_to=_resource_upload_to)
+
+    def save(self, *args, **kwargs):
+        created = not self.pk
+        super().save(*args, **kwargs)
+        if created:
+            _notify_resource_added(self.webapp)
 
 
 class WebApp(BaseModel):
@@ -12,7 +39,7 @@ class WebApp(BaseModel):
     title = models.CharField(max_length=140)
     slug = models.SlugField(unique=True, max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
-    script = models.FileField(upload_to="webapps/", storage=OverwriteStorage())
+    script = models.FileField(upload_to=WEBAPPS_UPLOAD_PATH, storage=OverwriteStorage())
     content_html = models.TextField(
         blank=True,
         help_text="The main body of the webapp in HTML",
@@ -25,8 +52,28 @@ class WebApp(BaseModel):
 
         super().save(*args, **kwargs)
 
+        if self.resources.all().exists():
+            # If webapp has additional resources, move it to same directory.
+            f = self.script
+            move_to = os.path.join(
+                _webapp_resource_dir(self.slug),
+                os.path.basename(f.name),
+            )
+            if f.path != move_to:
+                os.rename(
+                    f.path,
+                    move_to,
+                )
+                self.script = os.path.relpath(move_to, settings.MEDIA_ROOT)
+
+                super().save(*args, **kwargs)
+
     def __str__(self):
         return self.slug
 
     def get_absolute_url(self):
-        return reverse(view_names.WEBAPP, kwargs={"slug": self.slug})
+        return reverse(view_names.WEBAPP, args=[self.slug])
+
+
+def _notify_resource_added(webapp: WebApp):
+    webapp.save()
