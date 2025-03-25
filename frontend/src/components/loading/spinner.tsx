300c3c0d0a882b55aaecb68ad131061b2ba64342
Michael Beaton <beatonma@gmail.com>
2025-03-25 11:55:27 +0000

    Revert to old style of loading spinner.

--- frontend/src/components/loading/spinner.tsx
+++ frontend/src/components/loading/spinner.tsx
@@ -1,11 +1,17 @@
-import Icon from "@/components/icon";
 import { DivPropsNoChildren } from "@/types/react";
 import styles from "./spinner.module.css";
 
 export default function Spinner(props: DivPropsNoChildren) {
   return (
-    <div className={styles.loading} {...props}>
-      <Icon icon="MB" className={styles.loadingChild} />
+    <div className={styles.spinner} {...props}>
+      <div className={styles.spinnerWrapper}>
+        <div className={styles.spinnerChild} />
+        <div className={styles.spinnerChild} />
+        <div className={styles.spinnerChild} />
+      </div>
+      <noscript className="text-sm">
+        Loading error: javascript is disabled.
+      </noscript>
     </div>
   );
 }
