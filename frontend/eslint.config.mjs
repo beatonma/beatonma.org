8bccdee4a1e0699ab4ddd649c7e654c785f6f014
Michael Beaton <beatonma@gmail.com>
2026-02-18 11:06:07 +0000

    Removed unnecessary eslint plugins.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,6 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default defineConfig([
   {
     files: ["**/*.tsx", "**/*.t{s,sx}"],
     rules: {
@@ -21,6 +11,4 @@ const eslintConfig = [
       "@next/next/no-img-element": "off",
     },
   },
-];
-
-export default eslintConfig;
+]);
